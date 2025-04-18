3dbd5c7bc5789fd945476efd142c743af9ac453f
wilsone24 <wilsone@uninorte.edu.co>
2025-04-17 19:00:05 -0500

    refactor: remove unnecessary elements from Public component and update button styles in Register component

--- src/components/public/index.tsx
+++ src/components/public/index.tsx
@@ -5,8 +5,6 @@ import Register from "./register";
 function Public() {
   return (
     <div>
-      <h1>Public</h1>
-      <p>This is a public component.</p>
       <Register></Register>
     </div>
   );
