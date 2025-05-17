b671f764018d6b1e714134748f5ec58e37994612
Darwin Charris <hdarwin@uninorte.edu.co>
2025-05-17 16:24:05 -0500

    Sentry and CI

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://9323961bbe2f8b41253baa9095204ac1@o4509340464775168.ingest.us.sentry.io/4509340468838400",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
